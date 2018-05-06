f6787b193553ed0e54184b0e4e86319c366a2274
jungilwoo93 <panpan9311@gmail.com>
2018-05-06 16:33:53 +0200

    changer layout de img, (les postions d'autre objets ca depend il y a une img ou pas) et ajouter les questions de films

--- src/fr/ensisa/supercerveau/model/questions/QuestionFactory.java
+++ src/fr/ensisa/supercerveau/model/questions/QuestionFactory.java
@@ -8,7 +8,7 @@ public class QuestionFactory {
 		Question question;
 		switch(categorie)
 		{
-			case 0 : question = new QuestionRaceChien();
+			case 0 : question = new QuestionFilm();
 			break;
 			case 7: question = new QuestionArtist();
 			break;
